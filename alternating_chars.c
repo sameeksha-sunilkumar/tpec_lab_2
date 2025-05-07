1916e4d15603f9a66a5aacb84a20bf7e8383b19d
Sameeksha <160415587+sameeksha-sunilkumar@users.noreply.github.com>
2025-05-07 20:55:09 +0530

    additional 4th program

---
+++ alternating_chars.c
@@ -0,0 +1,34 @@
+#include <stdio.h>
+#include <string.h>
+#include <stdlib.h>
+
+int main() {
+    int t;
+    unsigned int count = 0;
+    char *c;
+
+    scanf("%d", &t);
+
+    c = (char *)malloc(sizeof(char) * 100002);
+    if (c == NULL) {
+        printf("Memory allocation failed\n");
+        return 1;
+    }
+
+    while (t--) {
+        scanf("%s", c);
+
+        count = 0;
+
+        for (int i = 0; c[i] != '\0'; i++) {
+            if (c[i] == c[i + 1]) {
+                count++;  
+            }
+        }
+
+        printf("%u\n", count);
+    }
+    free(c);
+
+    return 0;
+}
