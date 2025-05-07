ef61fbce28469aa3c8023ca59a656d0ed9a0912c
Sameeksha <160415587+sameeksha-sunilkumar@users.noreply.github.com>
2025-05-07 23:54:10 +0530

    regular 5th program

--- pangramss.c
+++ pangramss.c
@@ -1,30 +1,4 @@
-#include <stdio.h>
-#include <string.h>
-#include <ctype.h>
-
-int main() {
-    char s[10000];
-    int f[26] = {0}, ans = 0;
-
-    fgets(s, sizeof(s), stdin);
-
-    for (int i = 0; s[i]; i++) {
-        if (isalpha(s[i])) {
-            char ch = toupper(s[i]);
-            if (!f[ch - 'A']++) 
-                ans++;
-        }
-    }
-
-    if (ans == 26)
-        printf("pangram\n");
-    else
-        printf("not pangram\n");
-
-    return 0;
-}
-
-//OR (study for loop only)
+//(study for loop only)
 
 #include <stdio.h> 
 #include <string.h>
