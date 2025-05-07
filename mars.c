b281a5c97027728c54b333d0bb36d5ef9c31ce87
Sameeksha <160415587+sameeksha-sunilkumar@users.noreply.github.com>
2025-05-07 20:45:29 +0530

    additional 2nd program

---
+++ mars.c
@@ -0,0 +1,23 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+int main() {
+    char* S = (char *)malloc(10240 * sizeof(char));  
+    scanf("%s", S);  
+    int count = 0, i;
+    for(i = 0; S[i] != '\0'; i += 3) {  
+        if(S[i] != 'S') {
+            count++;
+        }
+        if(S[i + 1] != 'O') {
+            count++;
+        }
+        if(S[i + 2] != 'S') {
+            count++;
+        }
+    }
+
+    printf("%d", count);  
+    free(S);  
+    return 0;
+}
